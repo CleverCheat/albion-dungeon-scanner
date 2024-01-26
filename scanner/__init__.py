4050033a097b3e95bcb3d8b9dd1183cf719abd92
grantUser <grantuser@skiff.com>
2024-01-26 18:46:47 +0100

    [REFACTO] remove need of Template folder

--- scanner/__init__.py
+++ scanner/__init__.py
@@ -1,104 +1,121 @@
+import glob
 import os
 import shutil
 import xml.etree.ElementTree as ET
 
 from rich.console import Console
+
+from scanner.utils.binary import Binary
 from scanner.utils.config import Config
 
-prefab_files = []
-dungeon_skip_template = ["BACKDROP", "MASTER"]
 
-console = Console()
+class AlbionDungeonScanner:
+    def __init__(self) -> None:
+        self.config = Config
+        self.console = Console()
+        self.used_files = []
+
+    @property
+    def albion_dir(self):
+        if self.config.get("ao-dir"):
+            return (
+                self.config.get("ao-dir")
+                + r"Albion-Online_Data\StreamingAssets\GameData\templates\GREEN"
+            )
+
+        raise "You must enter the path to your game in the .env file."
+
+    def check_file(self, file_path):
+        filename = os.path.basename(file_path)
+
+        try:
+            shutil.move(
+                os.path.join(self.albion_dir, filename),
+                os.path.join(self.albion_dir + "\\.temp\\", filename),
+            )
+        except IOError:
+            self.used_files.append(file_path)
+
+    def restore_file(self, file_path):
+        filename = os.path.basename(file_path)
+
+        try:
+            shutil.move(
+                os.path.join(self.albion_dir + "\\.temp\\", filename),
+                os.path.join(self.albion_dir, filename),
+            )
+        except Exception:
+            pass
+
+    def run(self):
+        temp_path = self.albion_dir + "\\.temp\\"
+        if os.path.exists(temp_path):
+            shutil.rmtree(temp_path)
+        os.mkdir(temp_path)
+
+        files = glob.glob(os.path.join(self.albion_dir, "*.bin"))
+
+        for file in files:
+            self.check_file(file)
+            self.restore_file(file)
+
+        special_boss = []
+        chest_and_boss_list = []
+        flag7 = False
+        for file in self.used_files:
+            filename = os.path.basename(file)
+
+            if "Uncle" in filename:
+                special_boss.append("[red]Uncle Frost[/] [blue]( Blue Chest )[/]")
+
+            if "Anniversary" in file:
+                special_boss.append("[yellow]Anniversary Titan Boss[/]")
+
+            if not any(
+                skip_template in filename for skip_template in ["BACKDROP", "MASTER"]
+            ):
+                if "EXIT" in filename:
+                    flag7 = True
+
+                try:
+                    binary = Binary()
+                    decrypted_bytes = binary.decrypter.decrypt_binary_file(file)
+                    decrypted_str = decrypted_bytes.decode("utf-8")
+
+                    root = ET.fromstring(decrypted_str)
+
+                    layer_nodes = root.findall(".//layer")
+                    chest_types = []
+
+                    for layer_node in layer_nodes:
+                        if "reward_solo" in layer_node.attrib["name"].lower():
+                            tile_nodes = layer_node.findall(".//tile")
+                            chest_types = [tile.attrib["name"] for tile in tile_nodes]
+                            break
+
+                    if chest_types:
+                        chest_and_boss_list.append(f"({' , '.join(chest_types)})")
+                except Exception as e:
+                    print(e)
+                    pass
+
+        chest_and_boss_str = ", ".join(chest_and_boss_list)
+        if special_boss:
+            special_boss_str = ", ".join(special_boss)
+            self.console.print(f"[blue]   Event Boss      :[/]   {special_boss_str}")
+
+        if not chest_and_boss_list:
+            chest_and_boss_str = "[red]No Boss and Chests[/]"
+
+        self.console.print(f"[blue]   Chest Possibility : [/]   {chest_and_boss_str}")
+        self.console.print("   ")
+        self.console.print(
+            "[blue]   Floor           :[/]   Go to Next Floor"
+            if flag7
+            else "   [blue]   Floor           :[/]   End of Dungeon"
+        )
 
 
 def main():
-    albion_dir = Config.get("ao-dir") + r"Albion-Online_Data\StreamingAssets\GameData\templates\GREEN"
-    second_folder_archiv = Config.get("ao-dir") + r"Albion-Online_Data\StreamingAssets\GameData\templates\GREEN\NewFolder"
-
-    if not os.path.exists(second_folder_archiv):
-        os.makedirs(second_folder_archiv)
-
-    files = [
-        f for f in os.listdir("Template") if os.path.isfile(os.path.join("Template", f))
-    ]
-
-    for file in files:
-        try:
-            new_file = file.replace(".xml", ".bin")
-            shutil.move(
-                os.path.join(albion_dir, new_file),
-                os.path.join(second_folder_archiv, new_file),
-            )
-        except IOError as e:
-            prefab_files.append(file)
-
-    for file in files:
-        try:
-            new_file = file.replace(".xml", ".bin")
-            shutil.move(
-                os.path.join(second_folder_archiv, new_file),
-                os.path.join(albion_dir, new_file),
-            )
-        except IOError as e:
-            pass
-
-    check_chest()
-
-
-def check_chest():
-    files = [f for f in os.listdir("Template") if f.endswith(".xml")]
-    special_boss = []
-    flag7 = False
-    chest_and_boss_list = []
-
-    for template in prefab_files:
-        if "Uncle" in template:
-            special_boss.append("[red]Uncle Frost[/] [blue]( Blue Chest )[/]")
-
-        if "Anniversary" in template:
-            special_boss.append("[yellow]Anniversary Titan Boss[/]")
-
-        if not any(
-            skip_template in template for skip_template in dungeon_skip_template
-        ):
-            if "EXIT" in template:
-                flag7 = True
-
-            try:
-                file_info = next((f for f in files if template in f), None)
-                tree = ET.parse(os.path.join("Template", file_info))
-                root = tree.getroot()
-
-                layer_nodes = root.findall(".//layer")
-                chest_types = []
-
-                for layer_node in layer_nodes:
-                    if "reward_solo" in layer_node.attrib["name"].lower():
-                        tile_nodes = layer_node.findall(".//tile")
-                        chest_types = [tile.attrib["name"] for tile in tile_nodes]
-                        break
-
-                if chest_types:
-                    chest_and_boss_list.append(f"({' , '.join(chest_types)})")
-            except Exception as e:
-                console.print(f"Error : {e}", style="red")
-
-    chest_and_boss_str = ", ".join(chest_and_boss_list)
-    console.print("")
-
-    if special_boss:
-        special_boss_str = ", ".join(special_boss)
-        console.print(f"[blue]   Event Boss      :[/]   {special_boss_str}")
-
-    if not chest_and_boss_list:
-        chest_and_boss_str = "[red]No Boss and Chests[/]"
-
-    console.print(f"[blue]   Chest Possibility : [/]   {chest_and_boss_str}")
-    console.print("   ")
-    console.print(
-        "[blue]   Floor           :[/]   Go to Next Floor"
-        if flag7
-        else "   [blue]   Floor           :[/]   End of Dungeon"
-    )
-
-    input()
+    scanner = AlbionDungeonScanner()
+    scanner.run()
