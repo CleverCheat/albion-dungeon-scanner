2d56fa8dde31bcd7063947a6141163fb5d54a990
grant_user <grantuser@skiff.com>
2024-01-26 11:44:26 +0100

    [UPDATE] project structure

--- scanner/__init__.py
+++ scanner/__init__.py
@@ -1,41 +1,49 @@
 import os
 import shutil
 import xml.etree.ElementTree as ET
+
 from rich.console import Console
-from rich.table import Table
+from scanner.utils.config import Config
 
 prefab_files = []
-dungeon_skip_template = [
-    "BACKDROP",
-    "MASTER"
-]
+dungeon_skip_template = ["BACKDROP", "MASTER"]
 
 console = Console()
 
+
 def main():
-    albion_dir = r"D:\SteamLibrary\steamapps\common\Albion Online\game\Albion-Online_Data\StreamingAssets\GameData\templates\GREEN"
-    second_folder_archiv = r"D:\SteamLibrary\steamapps\common\Albion Online\game\Albion-Online_Data\StreamingAssets\GameData\templates\GREEN\NewFolder"
+    albion_dir = Config.get("ao-dir") + r"Albion-Online_Data\StreamingAssets\GameData\templates\GREEN"
+    second_folder_archiv = Config.get("ao-dir") + r"Albion-Online_Data\StreamingAssets\GameData\templates\GREEN\NewFolder"
 
     if not os.path.exists(second_folder_archiv):
         os.makedirs(second_folder_archiv)
 
-    files = [f for f in os.listdir("Template") if os.path.isfile(os.path.join("Template", f))]
+    files = [
+        f for f in os.listdir("Template") if os.path.isfile(os.path.join("Template", f))
+    ]
 
     for file in files:
         try:
             new_file = file.replace(".xml", ".bin")
-            shutil.move(os.path.join(albion_dir, new_file), os.path.join(second_folder_archiv, new_file))
+            shutil.move(
+                os.path.join(albion_dir, new_file),
+                os.path.join(second_folder_archiv, new_file),
+            )
         except IOError as e:
             prefab_files.append(file)
 
     for file in files:
         try:
             new_file = file.replace(".xml", ".bin")
-            shutil.move(os.path.join(second_folder_archiv, new_file), os.path.join(albion_dir, new_file))
+            shutil.move(
+                os.path.join(second_folder_archiv, new_file),
+                os.path.join(albion_dir, new_file),
+            )
         except IOError as e:
             pass
 
     check_chest()
+
 
 def check_chest():
     files = [f for f in os.listdir("Template") if f.endswith(".xml")]
@@ -50,7 +58,9 @@ def check_chest():
         if "Anniversary" in template:
             special_boss.append("[yellow]Anniversary Titan Boss[/]")
 
-        if not any(skip_template in template for skip_template in dungeon_skip_template):
+        if not any(
+            skip_template in template for skip_template in dungeon_skip_template
+        ):
             if "EXIT" in template:
                 flag7 = True
 
@@ -63,9 +73,9 @@ def check_chest():
                 chest_types = []
 
                 for layer_node in layer_nodes:
-                    if "reward_solo" in layer_node.attrib['name'].lower():
+                    if "reward_solo" in layer_node.attrib["name"].lower():
                         tile_nodes = layer_node.findall(".//tile")
-                        chest_types = [tile.attrib['name'] for tile in tile_nodes]
+                        chest_types = [tile.attrib["name"] for tile in tile_nodes]
                         break
 
                 if chest_types:
@@ -73,11 +83,11 @@ def check_chest():
             except Exception as e:
                 console.print(f"Error : {e}", style="red")
 
-    chest_and_boss_str = ', '.join(chest_and_boss_list)
+    chest_and_boss_str = ", ".join(chest_and_boss_list)
     console.print("")
 
     if special_boss:
-        special_boss_str = ', '.join(special_boss)
+        special_boss_str = ", ".join(special_boss)
         console.print(f"[blue]   Event Boss      :[/]   {special_boss_str}")
 
     if not chest_and_boss_list:
@@ -85,6 +95,10 @@ def check_chest():
 
     console.print(f"[blue]   Chest Possibility : [/]   {chest_and_boss_str}")
     console.print("   ")
-    console.print("[blue]   Floor           :[/]   Go to Next Floor" if flag7 else "   [blue]   Floor           :[/]   End of Dungeon")
+    console.print(
+        "[blue]   Floor           :[/]   Go to Next Floor"
+        if flag7
+        else "   [blue]   Floor           :[/]   End of Dungeon"
+    )
 
     input()
